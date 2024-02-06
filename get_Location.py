9430c0083f3da56893543c4d721f980371bd5a03
TheActualSanny <148360536+TheActualSanny@users.noreply.github.com>
2024-02-06 20:30:25 +0400

    Update get_Location.py

--- get_Location.py
+++ get_Location.py
@@ -1,3 +1,5 @@
+'''FETCHES CURRENT LOCATION FROM THE API'''
+
 import requests
 import json
 import pandas as pd
@@ -6,7 +8,7 @@ url = "https://api.bigdatacloud.net/data/reverse-geocode-client"
 payload = {}
 headers = {}
 
-def Location(df):
+def get_location(df):
     params = {'latitude' : df.iloc[-1]['latitude'], 'longitude' : df.iloc[-1]['longitude'], 'localityLanguage' : 'en'}
     response = requests.request("GET", url, headers=headers, data=payload, params=params)
     data = json.loads(response.text)
