862f71fd81332955ed9b06ecc7597593cf7b3d9f
TheActualSanny <148360536+TheActualSanny@users.noreply.github.com>
2024-02-21 01:02:08 +0400

    Update write_to_table.py

--- write_to_table.py
+++ write_to_table.py
@@ -1,29 +1,14 @@
 import psycopg2
 import pandas
+import needed_queries as const
 
-def write(location, distance, row, conn, curr):
+def write(location, distance, row, conn, curr) -> None: #must add into seperate file as dictionary
     with conn:
-        curr.execute('''CREATE TABLE IF NOT EXISTS iss_normalized (
-                name text,
-                id integer,
-                latitude real,
-                longitude real,
-                altitude real,
-                velocity real,
-                visibility text,
-                footprint real,
-                timestamp integer,
-                daynum real,
-                solar_lat real,
-                solar_lon real,
-                units text,
-                aprox_distance real,
-                curr_location text   
-        ) PARTITION BY LIST(visibility)''')
-
+        curr.execute('''CREATE TABLE IF NOT EXISTS iss_normalized ({}) PARTITION BY LIST(visibility)'''.format(' '.join([' '.join(inst) for inst in const.create_normalized.items()]))) ## check documentation
         curr.execute('''CREATE TABLE IF NOT EXISTS iss_day PARTITION OF iss_normalized FOR VALUES IN ('daylight')''')
         curr.execute('''CREATE TABLE IF NOT EXISTS iss_eclipsed PARTITION OF iss_normalized FOR VALUES IN ('eclipsed')''')
         curr.execute('''INSERT INTO iss_normalized VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                    ''', (row['name'], int(row['id']), float(row['latitude']), row['longitude'], row['altitude'], row['velocity'], row['visibility'],
-                        row['footprint'], int(row['timestamp']), row['daynum'], row['solar_lat'], row['solar_lon'], row['units'],
-                        float(distance), str(location)))
+                    ''', const.normalized_values(row, distance, location)) #must add into seperate file, add an unpacking operator
+    conn.close()
+    curr.close()
+    
